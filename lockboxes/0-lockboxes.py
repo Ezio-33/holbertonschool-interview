274b500957a60e92ca75951dd5928a40d0700f79
Ezio-33 <8691@holbertonstudents.com>
2025-01-03 17:41:26 +0100

    algo

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
