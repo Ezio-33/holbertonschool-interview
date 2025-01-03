804d07cc0bf173ecfba00b368c24114be367fc3f
Ezio-33 <8691@holbertonstudents.com>
2025-01-03 17:45:23 +0100

    Ajout de documentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """
+    Détermine si toutes les boîtes peuvent être déverrouillées
+     en partant de la première boîte.
+
+    Args:
+        boxes (List[List[int]]): Une liste de boîtes, où
+          chaque boîte contient une liste de clés.
+
+    Returns:
+        bool: Vrai si toutes les boîtes peuvent être déverrouillées, Faux sinon.
+    """
     n = len(boxes)
     opened = [False] * n
     opened[0] = True
