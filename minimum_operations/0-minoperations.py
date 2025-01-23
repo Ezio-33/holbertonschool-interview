64810032dd3d5bf635c162e1e659342278a03d10
Ezio-33 <8691@holbertonstudents.com>
2025-01-23 19:58:39 +0100

    Ajout des retour a la ligne pour PEP8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,32 +4,33 @@ Module qui calcule le nombre minimum d'opérations
 pour obtenir n caractères 'H'
 """
 
+
 def minOperations(n):
     """
     Calcule le nombre minimum d'opérations nécessaires
     pour obtenir exactement n caractères 'H'
-    
+
     Args:
         n (int): nombre de caractères 'H' souhaités
-        
+
     Returns:
         int: nombre minimum d'opérations nécessaires
     """
     if n <= 1:
         return 0
-        
+
     operations = 0
     diviseur = 2
-    
+
     while n > 1:
         while n % diviseur == 0:
             operations += diviseur
             n = n // diviseur
         diviseur += 1
-        
+
         if diviseur * diviseur > n:
             if n > 1:
                 operations += n
                 break
-                
+
     return operations
