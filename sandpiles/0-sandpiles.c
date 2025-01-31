ee8cbd5c119745800f3a69f8559f0d9ea089d378
Ezio-33 <8691@holbertonstudents.com>
2025-01-31 13:59:36 +0100

    task 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,9 +1,9 @@
-#include "sandpiles.h"
 #include <stdio.h>
+#include "sandpiles.h"
 
 /**
  * print_grid - Affiche une grille 3x3
- * @grid: Grille 3x3 à afficher
+ * @grid: Grille à afficher
  */
 static void print_grid(int grid[3][3])
 {
@@ -13,7 +13,7 @@ static void print_grid(int grid[3][3])
     {
         for (j = 0; j < 3; j++)
         {
-            if (j > 0)
+            if (j)
                 printf(" ");
             printf("%d", grid[i][j]);
         }
@@ -22,12 +22,12 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * is_unstable - Vérifie si la grille est instable (au moins une cellule > 3)
+ * is_stable - Vérifie si le tas de sable est stable
  * @grid: Grille 3x3 à vérifier
  *
- * Return: 1 si instable, 0 sinon
+ * Return: 1 si stable (aucune cellule > 3), 0 sinon
  */
-static int is_unstable(int grid[3][3])
+static int is_stable(int grid[3][3])
 {
     int i, j;
 
@@ -36,59 +36,61 @@ static int is_unstable(int grid[3][3])
         for (j = 0; j < 3; j++)
         {
             if (grid[i][j] > 3)
-                return (1);
+                return (0);
         }
     }
-    return (0);
+    return (1);
 }
 
 /**
- * topple - Effectue l'effondrement pour toutes les cellules > 3
+ * topple - Fait basculer les grains de sable des cellules instables
  * @grid: Grille 3x3 à modifier
  */
 static void topple(int grid[3][3])
 {
     int i, j;
-    int temp[3][3];
+    int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-    /* Copie de la grille avant de redistribuer */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            temp[i][j] = grid[i][j];
-    }
-
-    /* Pour chaque cellule, si > 3, on retire 4 et on distribue aux voisins */
+    /* Distribution des grains */
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            if (temp[i][j] > 3)
+            if (grid[i][j] > 3)
             {
-                grid[i][j] -= 4;
+                temp[i][j] -= 4;
                 if (i > 0)
-                    grid[i - 1][j] += 1;
+                    temp[i - 1][j]++;
                 if (i < 2)
-                    grid[i + 1][j] += 1;
+                    temp[i + 1][j]++;
                 if (j > 0)
-                    grid[i][j - 1] += 1;
+                    temp[i][j - 1]++;
                 if (j < 2)
-                    grid[i][j + 1] += 1;
+                    temp[i][j + 1]++;
             }
+        }
+    }
+
+    /* Mise à jour de la grille avec les nouvelles valeurs */
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid[i][j] += temp[i][j];
         }
     }
 }
 
 /**
- * sandpiles_sum - Calcule la somme de deux grilles de sable et stabilise le résultat
- * @grid1: Première grille (et celle qui stocke le résultat final)
- * @grid2: Deuxième grille à additionner
+ * sandpiles_sum - Calcule la somme de deux tas de sable
+ * @grid1: Premier tas de sable (contiendra le résultat)
+ * @grid2: Deuxième tas de sable
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
     int i, j;
 
-    /* 1. Addition des deux grilles */
+    /* Addition des deux grilles */
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
@@ -97,11 +99,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
         }
     }
 
-    /* 2. Tant que la grille est instable, on l’affiche puis on la fait “toppler” */
-    while (is_unstable(grid1))
+    /* Stabilisation par basculement successif */
+    while (!is_stable(grid1))
     {
-        print_grid(grid1);
         printf("=\n");
+        print_grid(grid1);
         topple(grid1);
     }
 }
