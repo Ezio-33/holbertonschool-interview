326c1b31e400804b4ef7f8ee7d4c82af57a8214a
Ezio-33 <8691@holbertonstudents.com>
2025-02-14 00:43:34 +0100

    correction algo

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -11,26 +11,35 @@ lors d'une interruption clavier (CTRL+C).
 
 import sys
 
+if __name__ == '__main__':
 
-file_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-try:
-    for i, line in enumerate(sys.stdin, 1):
-        splited = line.split(" ")
-        if len(splited) < 2:
-            continue
-        if splited[-2] in status_codes:
-            status_codes[splited[-2]] += 1
-        file_size += eval(splited[-1])
-        if i % 10 == 0:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
-finally:
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
