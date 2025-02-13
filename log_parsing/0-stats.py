0d184ef55e652fe27b583a512c9e02a420c095c6
Ezio-33 <8691@holbertonstudents.com>
2025-02-14 00:34:11 +0100

    correction task algo

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Script qui lit des logs depuis stdin et affiche des statistiques toutes
-les 10 lignes ou lors d'une interruption clavier (CTRL+C).
+# -*- coding: utf-8 -*-
+"""
+Script qui lit des logs depuis l'entrée standard (stdin)
+et affiche des statistiques :
+- Taille totale des fichiers
+- Nombre de lignes par code HTTP valide
+Les statistiques sont affichées toutes les 10 lignes ou lors
+d'une interruption clavier (CTRL+C).
 """
 
 import sys
 
 # Variables globales
-file_size = 0  # Somme totale des tailles de fichiers
-status_codes = {
+taille_totale = 0  # Somme totale des tailles de fichiers
+codes_status = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -15,38 +21,38 @@ status_codes = {
     "403": 0,
     "404": 0,
     "405": 0,
-    "500": 0}  # Compteur par code HTTP
+    "500": 0}  # Compteur des codes HTTP valides
 
 try:
     # Lecture ligne par ligne depuis stdin
-    for i, line in enumerate(sys.stdin, 1):
+    for i, ligne in enumerate(sys.stdin, 1):
         # Découper la ligne en mots
-        splited = line.split(" ")
+        decoupe = ligne.split(" ")
 
         # Vérifier que la ligne est valide (au moins deux éléments)
-        if len(splited) < 2:
+        if len(decoupe) < 2:
             continue
 
         # Mettre à jour le compteur pour le code HTTP si valide
-        if splited[-2] in status_codes:
-            status_codes[splited[-2]] += 1
+        if decoupe[-2] in codes_status:
+            codes_status[decoupe[-2]] += 1
 
         # Ajouter la taille du fichier au total
         try:
-            file_size += int(splited[-1])
+            taille_totale += int(decoupe[-1])
         except ValueError:
             pass  # Ignorer les erreurs si la taille n'est pas un entier valide
 
         # Afficher les statistiques toutes les 10 lignes
         if i % 10 == 0:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
+            print("Taille totale: {}".format(taille_totale))
+            for code, valeur in sorted(codes_status.items()):
+                if valeur > 0:
+                    print("{}: {}".format(code, valeur))
 
 finally:
     # Afficher les statistiques finales avant de quitter
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+    print("Taille totale: {}".format(taille_totale))
+    for code, valeur in sorted(codes_status.items()):
+        if valeur > 0:
+            print("{}: {}".format(code, valeur))
