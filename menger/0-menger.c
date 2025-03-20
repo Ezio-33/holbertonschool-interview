8fed6658d16ca07077d3c811005dd40dbc7007c8
Ezio-33 <8691@holbertonstudents.com>
2025-03-21 00:17:17 +0100

    Correction betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -4,7 +4,7 @@
 #include "menger.h"
 
 /**
- * isHole - Fonction pour vérifier si une cellule doit être vide
+ * isHole - Vérifie si une cellule doit être vide
  * @row: Position de la ligne
  * @col: Position de la colonne
  *
@@ -12,51 +12,44 @@
  */
 int isHole(int row, int col)
 {
-    while (row > 0 || col > 0)
-    {
-        if (row % 3 == 1 && col % 3 == 1)
-        {
-            return (1);
-        }
-        row /= 3;
-        col /= 3;
-    }
-    return (0);
+	while (row > 0 || col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+		{
+			return (1);
+		}
+		row /= 3;
+		col /= 3;
+	}
+	return (0);
 }
 
 /**
- * menger - Fonction pour dessiner l'éponge de Menger
+ * menger - Dessine une éponge de Menger
  * @level: Niveau de récursion de l'éponge
  *
- * Description: Cette fonction génère une représentation ASCII
- * d'une éponge de Menger au niveau spécifié. Le niveau 0 est un
- * simple carré, et chaque niveau suivant subdivise chaque carré
- * en 9 carrés plus petits, en retirant le carré central.
+ * Description: Génère une représentation ASCII d'une éponge de Menger
+ * Le niveau 0 est un carré plein. Chaque niveau supérieur subdivise
+ * chaque carré en 9 sous-carrés avec le central vide.
  */
 void menger(int level)
 {
-    int size, i, j;
+	int size, i, j;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    /* Taille totale de l'éponge */
-    size = pow(3, level);
+	size = pow(3, level);
 
-    /* Parcours l'ensemble de l'éponge */
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            if (isHole(i, j))
-            {
-                printf(" ");
-            }
-            else
-            {
-                printf("#");
-            }
-        }
-        printf("\n");
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (isHole(i, j))
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
 }
