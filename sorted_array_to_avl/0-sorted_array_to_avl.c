ad709b261eaa7c3142b713c7310cd822ded54411
Ezio-33 <8691@holbertonstudents.com>
2025-04-02 10:09:59 +0200

    correction betty

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,23 +10,24 @@
  * @parent: Pointer to the parent node
  * Return: Pointer to the root node of the subtree
  */
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
+avl_t *sorted_array_to_avl_helper(int *array, int start,
+								  int end, avl_t *parent)
 {
-    if (start > end)
-        return (NULL);
+	if (start > end)
+		return (NULL);
 
-    int mid = (start + end) / 2;
-    avl_t *node = malloc(sizeof(avl_t));
+	int mid = (start + end) / 2;
+	avl_t *node = malloc(sizeof(avl_t));
 
-    if (!node)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    node->n = array[mid];
-    node->parent = parent;
-    node->left = sorted_array_to_avl_helper(array, start, mid - 1, node);
-    node->right = sorted_array_to_avl_helper(array, mid + 1, end, node);
+	node->n = array[mid];
+	node->parent = parent;
+	node->left = sorted_array_to_avl_helper(array, start, mid - 1, node);
+	node->right = sorted_array_to_avl_helper(array, mid + 1, end, node);
 
-    return (node);
+	return (node);
 }
 
 /**
@@ -37,8 +38,8 @@ avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
+	if (!array || size == 0)
+		return (NULL);
 
-    return (sorted_array_to_avl_helper(array, 0, size - 1, NULL));
+	return (sorted_array_to_avl_helper(array, 0, size - 1, NULL));
 }
