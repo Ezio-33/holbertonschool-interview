5f2de34e5b37009d3874dd19ea1492c37950d54b
Ezio-33 <8691@holbertonstudents.com>
2025-07-02 16:14:14 +0200

    Correction betty

--- double_circular_linked_list/list.h
+++ double_circular_linked_list/list.h
@@ -1,22 +1,22 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef _LIST_H_
+#define _LIST_H_
 
 /**
- * struct List - liste doublement chaînée
- * @str: chaîne de caractères - (chaîne allouée dynamiquement)
- * @prev: pointe vers le nœud précédent
- * @next: pointe vers le nœud suivant
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description: structure de nœud de liste doublement chaînée
- * pour le projet Holberton
+ * Description: doubly linked list node structure
  */
-typedef struct List {
-    char *str;
-    struct List *prev;
-    struct List *next;
+typedef struct List
+{
+char *str;
+struct List *prev;
+struct List *next;
 } List;
 
 List *add_node_end(List **list, char *str);
 List *add_node_begin(List **list, char *str);
 
-#endif 
+#endif
